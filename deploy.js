4c2ef7c40cfd86d00a345d57e2f6d3aef2b15411
ksinghj <kartarsjabanda@gmail.com>
2022-03-25 15:00:49 +0000

    deployed contract to rinkeby testnet

--- deploy.js
+++ deploy.js
@@ -1,13 +1,13 @@
+require('dotenv').config()
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
 const provider = new HDWalletProvider(
-  'REPLACE_WITH_YOUR_MNEMONIC',
-  // remember to change this to your own phrase!
-  'https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c'
-  // remember to change this to your own endpoint!
-);
+  process.env.MMFFSP,
+  'https://rinkeby.infura.io/v3/b63a5ee481ab46548f3cda9218d11170'
+)
+
 const web3 = new Web3(provider);
 
 const deploy = async () => {
@@ -19,6 +19,7 @@ const deploy = async () => {
     .deploy({ data: bytecode })
     .send({ gas: '1000000', from: accounts[0] });
 
+  console.log('interface: ', interface);
   console.log('Contract deployed to', result.options.address);
   provider.engine.stop();
 };
